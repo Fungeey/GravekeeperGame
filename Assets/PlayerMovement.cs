39c61f6c21bdcc22fc81de6dcb31ea73598619fb
Epon <ethan.wong@hotmail.com>
2018-04-02 21:41:24 +0000

    Improved player movement and input system

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,29 +4,66 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     [SerializeField]
-    private float speed = 0.5f;
+    private float movSpeed = 5f;
+    [SerializeField]
+    private float rotSpeed = 20f;
+
     private Vector3 targetPos;
+    private Quaternion targetRot;
+    
+    private bool up, down, left, right;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         targetPos = transform.position;
+        targetRot = transform.rotation;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if (Input.GetKeyDown(KeyCode.W) && transform.position == targetPos) {
-            targetPos += transform.up;
-        }
-        if (Input.GetKeyDown(KeyCode.A) && transform.position == targetPos) {
-            transform.Rotate(new Vector3(0, 0, 90));
-        }
-        if (Input.GetKeyDown(KeyCode.S) && transform.position == targetPos) {
-            targetPos -= transform.up;
-        }
-        if (Input.GetKeyDown(KeyCode.D) && transform.position == targetPos) {
-            transform.Rotate(new Vector3(0, 0, -90));
-        }
 
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed);
+    private void Update() {
+        up = Input.GetKeyDown(KeyCode.W);
+        left = Input.GetKeyDown(KeyCode.A);
+        down = Input.GetKeyDown(KeyCode.S);
+        right = Input.GetKeyDown(KeyCode.D);
+    }
+    
+    void FixedUpdate () {
+        bool canMove = transform.position == targetPos && transform.rotation == targetRot;
+
+        if (left && canMove) {
+            left = false;
+            RoundTransform();
+            targetRot.eulerAngles += new Vector3(0, 0, 90);
+        } else if (right && canMove) {
+            right = false;
+            RoundTransform();
+            targetRot.eulerAngles += new Vector3(0, 0, -90);
+        } else if (up && canMove) {
+            up = false;
+            RoundTransform();
+            targetPos += transform.up.normalized;
+        } else if (down && canMove) {
+            down = false;
+            RoundTransform();
+            targetPos -= transform.up.normalized;
+        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, movSpeed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotSpeed);
+
 	}
+
+    Vector3 SnapToGrid(Vector3 position) {
+        return new Vector3Int (Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.y), Mathf.RoundToInt(position.z));
+    }
+
+    Quaternion SnapToRight(Quaternion rotation) {
+        Vector3 angles = rotation.eulerAngles;
+        angles = new Vector3Int(Mathf.RoundToInt(angles.x), Mathf.RoundToInt(angles.y), Mathf.RoundToInt(angles.z));
+        return Quaternion.Euler(angles);
+    }
+
+    void RoundTransform() {
+        transform.position = SnapToGrid(transform.position);
+        transform.rotation = SnapToRight(transform.rotation);
+    }
 }
