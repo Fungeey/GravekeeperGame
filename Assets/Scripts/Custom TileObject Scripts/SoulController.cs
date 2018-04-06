a36653aba1c171c8601be9eaa48882cd2fbf2e00
Fungeey <aaron@abcrlee.com>
2018-04-06 14:51:04 +0000

    Tweaked lighting

--- Assets/Scripts/Custom TileObject Scripts/SoulController.cs
+++ Assets/Scripts/Custom TileObject Scripts/SoulController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Tilemaps;
 [RequireComponent(typeof(TileObject))]
 public class SoulController : MonoBehaviour {
     
-    public Tile gravestone;
+    public Tile fullGravestone;
     public GameController gameController;
     private TileObject soulTileObject;
 
@@ -18,13 +18,14 @@ public class SoulController : MonoBehaviour {
 
     private void Update() {
         // If you are on a gravestone
-        if (!soulTileObject.moving && soulTileObject.tilemaps[0].GetTile(soulTileObject.tilePos).name == "Gravestone") {
+        TileBase tile = soulTileObject.tilemaps[0].GetTile(soulTileObject.tilePos);
+        if (tile != null && !soulTileObject.moving && tile.name == "Gravestone") {
             // Check if all the gravestones are full
 
             // Then delete the gravestone
             soulTileObject.tilemaps[0].SetTile(soulTileObject.tilePos, null);
             // Put a gravestone in the Main (solid) layer
-            soulTileObject.tilemaps[1].SetTile(soulTileObject.tilePos, gravestone);
+            soulTileObject.tilemaps[1].SetTile(soulTileObject.tilePos, fullGravestone);
             // Destroy this soul
             Destroy(gameObject);
         }
