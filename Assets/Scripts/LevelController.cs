711daa2fb1fa16199c422dab9b84f1b3c2726f2d
Epon <ethan.wong@hotmail.com>
2018-04-08 18:27:28 +0000

    improved level select screen

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.GetChild(0).GetChild(0).GetComponent<Text>().text = gameObject.name;
+	}
+}
